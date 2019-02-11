927c2b874e106d50c2185d384eb02d544d8d10be
Yves Guidet <yves.guidet@gmail.com>
2019-02-11 19:58:40 +0100

    Validation initiale 	nouveau fichier : aBovo.mm 	nouveau fichier : aBovo.py

---
+++ aBovo.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1549909897463" ID="ID_186436443" MODIFIED="1549909897463" TEXT="aBovo">
+<node CREATED="1549909897463" ID="ID_1806820630" MODIFIED="1549909912462" POSITION="right" TEXT="lundi 11 f&#xe9;vrier 2019, 19:26:19 (UTC+0100)">
+<cloud/>
+</node>
+<node CREATED="1549909897463" ID="ID_196316649" MODIFIED="1549909897463" POSITION="right" TEXT="lun*"/>
+<node CREATED="1549909897463" ID="ID_1058138746" LINK="/home/yves/2011/dev/Python/outils/giclee/aBovo" MODIFIED="1549909897463" POSITION="right" TEXT="dir"/>
+<node CREATED="1549909937254" ID="ID_1708016124" MODIFIED="1549909979032" POSITION="left" TEXT="g&#xe9;n&#xe9;ration de la sem suiv; &#xe0; partir de rien"/>
+</node>
+</map>
---
+++ aBovo.py
@@ -0,0 +1,4 @@
+#!  /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#	lundi 11 février 2019, 19:26:19 (UTC+0100)
