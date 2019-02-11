4674396cf9d813ea2992464aab597241b94eb467
Yves Guidet <yves.guidet@gmail.com>
2019-02-11 21:08:53 +0100

    modifié :         aBovo.py 	nouveau fichier : bugGicl.pyc

--- aBovo.py
+++ aBovo.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 #	lundi 11 février 2019, 19:26:19 (UTC+0100)
+# 	génération de la sem suiv. à partir de rien
+
+def aBovo():
+	''' '''
+	from bugGicl import dirCartes, numSemCour
+	n = numSemCour()
+	
+	assert 0, n
+
+if __name__ == '__main__':
+	aBovo()
