f4a30816aa1347606024897c9c7e2305e0174106
Yves Guidet <yves.guidet@gmail.com>
2019-02-13 09:12:45 +0100

    avance 	modifié :         aBovo.py

--- aBovo.py
+++ aBovo.py
@@ -1,11 +1,31 @@
 #!  /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#	lundi 11 février 2019, 19:26:19 (UTC+0100)
+#	du 11 février 2019, 19:26:19 (UTC+0100)
 # 	génération de la sem suiv. à partir de rien
 
 #	/home/yves/2011/dev/Python/outils/pyNewMap/
 from newMap import insDir
+
+def insDu(c):
+	''' insère date '''
+	#	grep -ni insdir /media/home/yves/2011/dev/Python/outils/pyNewMap/newMap.py
+	#	434:def insDir(c):
+
+	from semaineSuivante import lundiProchain
+
+	du = str(lundiProchain())
+	du = du.split('-')
+
+	q = du[2]
+	numMois = int(du[1])
+
+	m = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'decembre'][numMois]
+
+	if True:
+		print 'insDu(c) : m = {}'.format(m)
+	a = du[0]	# year
+	assert 0, (q, m, a)
 
 def aBovo():
 	''' '''
@@ -17,8 +37,9 @@ def aBovo():
 	nomc = 'Sem{:02d}{}'.format(n, an)
 
 	import newMap
-	
+
 	c = newMap.main(nombase = nomc, dater = False, leDir = '.', over = True)
+	insDu(c)
 	insDir(c)
 	assert 0, c
 
